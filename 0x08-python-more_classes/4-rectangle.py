94e27da240ff1bd39685a8f1a651fd8a2bd0a087
rexmannII <onyegbulerexejike@gmail.com>
2024-04-08 17:28:53 -0700

    updated

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -41,7 +41,7 @@ class Rectangle:
         self.__height = value
     def area(self):
         """returns the area of the rectangle"""
-        return self.__width * self>__height
+        return self.__width * self.__height
 
     def perimeter(self):
         """returns the perimeter of the rectangle"""
