88f49299596519f59204e809f9af35cd6c347f1e
rexmannII <onyegbulerexejike@gmail.com>
2024-04-21 08:53:37 -0700

    adding a dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,5 +101,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         '''Returns dictionary representation of ths class.'''
-        return ("id": self.id, "width": self.__width, "height": self.__height, "x": self.__x, "y": self.__y)
+        return {"id": self.id, "width": self.__width, "height": self.__height, "x": self.__x, "y": self.__y}
 
