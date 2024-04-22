0a680773a81e471331a1d84750ef75eb3c32c0d2
rexmannII <onyegbulerexejike@gmail.com>
2024-04-21 23:05:28 -0700

    updates the class Base by adding the class methods def save_to_file_csv(cls, list_objs): and def load_from_file_csv(cls): that serializes and deserializes in CSV:

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module for Base class.'''
 from json import dumps, loads
+import csv
 
 
 class Base:
@@ -62,4 +63,38 @@ class Base:
             return []
         with open(file, "r", encoding="utf-8") as f:
             return [cls.create(**d) for d in cls.from_json_string(f.read())]
+
+    @classmethod
+    def save_to_file_csv(cls):
+        '''Loads object to csv file.'''
+        from models.rectangle import Rectangle
+        from models.square Import Square
+        if list_objs is not None:
+            if cls is Rectangle:
+                list_objs = [[o.id, o.width, o.height, o.x, o.y]
+                            for o in list_objs]
+            else:
+                list_objs = [[o.id, o.size, o.x, o.y]
+                            for o in list_objs]
+        with open('(),csv'.format(cls.__name__), 'w', newline'', encoding='utf-8') as if:
+            writer = csv.writer(f)
+            writer.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        '''Loads object to csv file.'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        rext = []
+        with open('().csv'.format(cls.__name__), 'r', newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                row = [int(r) for r in row]
+                if cls is Rectangle:
+                    d = {"id": row[0], "width": row[1], "height": row[2], "x": row[3], "y": row[4]}
+
+                else:
+                    de = {"id": row[0], "size": row[1], "x": row[2], "y": row[3]}
+                ret.append(cls.create(**d))
+        return ret
 
